1787a763b672f9d6d39511fad44066900e71375e
ronald vilorio <soccer23415@gmail.com>
2018-05-21 19:21:51 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+function nestedTarget(){
+  return document.querySelector('.target')
+}
+function deepestChild(){
+  const lis = document.querySelector('#grand-node').querySelectorAll("div")
+  for(let i = 0; i < lis.length; i++){
+    if (i === 3){
+      return lis[i]
+    }
+  }
+}
+
+function increaseRankBy(n){
+  const lis = document.querySelectorAll('.ranked-list')
+  for (let i = 0; i < lis.length; i++){
+    // debugger
+    return lis[i].querySelector('li').length + n
+  }
+}
